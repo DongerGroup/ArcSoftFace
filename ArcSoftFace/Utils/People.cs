63553626511a03a5d5ce8e313c0f76638087ce47
DongerGroup <35094427+DongerGroup@users.noreply.github.com>
2019-01-03 13:03:58 +0800

    添加项目文件。

---
+++ ArcSoftFace/Utils/People.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcSoftFace.Utils
+{
+    public class People
+    {
+        public string name;
+        public Dictionary<string, IntPtr> images;
+        public People()
+        {
+            name = null;
+            images = null;
+        }
+        public People(string _name, KeyValuePair<string, IntPtr> keyValuePair)
+        {
+            name = _name;
+            images = null;
+            images.Add(keyValuePair);
+        }
+        public void Add(KeyValuePair<string, IntPtr> keyValuePair)
+        {
+            images.Add(keyValuePair);
+        }
+    }
+}
